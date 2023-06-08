0811f0fb87f24fa387e4680c9f877d09c5cc494b
Sisasoft Proje Yönetimi <info@sisasoft.com.tr>
2023-06-08 15:34:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -741,3 +741,12 @@ do_install() {
 # wrapped up in a function so that we have some protection against only getting
 # half the file during "curl | sh"
 do_install
+
+printf 'Waiting for Docker to start...\n\n'
+sleep 5
+
+# Docker Compose
+sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
+printf '\nDocker Compose installed successfully\n\n'
